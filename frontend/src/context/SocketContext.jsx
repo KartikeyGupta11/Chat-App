c8fcc986c77df2e9ecb9bec9919db4e937a992dc
Kartikey <g.kartikey.1104@gmail.com>
2024-08-02 16:31:10 +0530

    Small Commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("https://chat-app-production-x7rz.onrender.com/",{
+            const socket = io("https://chat-app-production-x7rz.onrender.com",{
                 query:{
                     userId: authUser._id
                 }
